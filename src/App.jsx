dd16ae676b2d50c6ae637235fc91b6d9585b7606
salmanbangash <salmanbangsh18@gmail.com>
2026-06-18 12:46:11 +0500

    Add testimonials section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+
 import Navbar from './component/Navbar/navbar'
 import Hero from './component/Hero/Hero'
 import Program from './component/Program/program'
 import Title from './component/Title/Title'
 import About from './component/About/About'
 import Gallery from './component/Gallery/Gallery'
+import Testimonials from './component/Testimonials/Testimonials'
 
 
 const App = () => {
@@ -18,9 +20,11 @@ const App = () => {
       <Title subTitle='Our PROGRAM' title='What We Offer'/>
       <Program/>
       <About/>
+      <Title subTitle='Gallery' title='Campus Photos'/>
       <Gallery/>
       <Title subTitle='TESTIMONIALS' title='What Student Says'/>
       </div>
+     <Testimonials/>
 
     </div>
   )
